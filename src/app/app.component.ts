9cfb8ecb649850a7aea09430255cb1158116cd10
Ihnat Mikhalkovich <ihnat.mikhalkovich@gmail.com>
2021-05-02 23:56:38 +0300

    First commit.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Reservation, ReservationRequest, ReservationService } from './reservation.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,76 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'reservation-app';
+
+  constructor(private reservationService: ReservationService) {}
+
+  rooms!: Room[];
+  roomSearchForm!: FormGroup;
+  currentCheckInVal!: string;
+  currentCheckOutVal!: string;
+  currentRoomNumber!: number;
+  currentPrice!: number;
+  currentReservations!: Reservation[];
+
+  ngOnInit() {
+    this.roomSearchForm = new FormGroup({
+      checkIn: new FormControl(''),
+      checkOut: new FormControl(''),
+      roomNumber: new FormControl('')
+    });
+    
+    this.roomSearchForm.valueChanges.subscribe(form => {
+      this.currentCheckInVal = form.checkIn;
+      this.currentCheckOutVal = form.checkOut;
+
+      if (form.roomNumber) {
+        let roomValues: string[] = form.roomNumber.split('|');
+        this.currentRoomNumber = Number(roomValues[0]);
+        this.currentPrice = Number(roomValues[1]);
+      }
+    });
+
+    this.rooms = [
+      new Room("127", "127", "150"),
+      new Room("138", "138", "180"),
+      new Room("254", "254", "200")
+    ];
+
+    this.findAllReservations();
+  }
+
+  createReservation() {
+    let reservationRequest = new ReservationRequest(
+      this.currentRoomNumber,
+      this.currentCheckInVal,
+      this.currentCheckOutVal,
+      this.currentPrice
+    );
+
+    this.reservationService.createReservation(reservationRequest)
+      .subscribe(postResult => {
+        console.log(postResult);
+        this.findAllReservations();
+      });
+  }
+
+  findAllReservations() {
+    this.reservationService.findAllReservations()
+      .subscribe(getResult => {
+        console.log(getResult);
+        this.currentReservations = getResult
+      });
+  }
+}
+
+export class Room {
+  id: string;
+  roomNumber: string;
+  price: string;
+
+  constructor(id: string, roomNumber: string, price: string) {
+    this.id = id;
+    this.roomNumber = roomNumber;
+    this.price = price;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './app-routing.module'; 
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule, 
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
